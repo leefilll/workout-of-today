8e61ede9d4d4201670ea9fc88067cf99bbc96537
Lee <poleewer@gmail.com>
2020-04-07 22:48:32 +0900

    first commit

---
+++ WorkoutOfToday/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  WorkoutOfToday
+//
+//  Created by Lee on 2020/04/07.
+//  Copyright © 2020 Lee. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+    // MARK: UISceneSession Lifecycle
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+}
+
---
+++ WorkoutOfToday/TodayWorkoutViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkoutOfToday
+//
+//  Created by Lee on 2020/04/07.
+//  Copyright © 2020 Lee. All rights reserved.
+//
+
+import UIKit
+
+class TodayWorkoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
